260e0a421bdef5491aa1094459ffd1d39ad591d4
Leandro <souzadeoliveiraleandro@gmail.com>
2025-04-10 19:48:28 -0300

    atualizações

--- app/index.tsx
+++ app/index.tsx
@@ -4,16 +4,18 @@ import { Button, Text, View } from "react-native";
 
 export default function HomeScreen(){
     const STARTVALUE = 0;
+    const nome = "leandro";
     const [count,setCount] = useState(STARTVALUE);
+
     
 
 
     return (
         <View>
-            <Text>Bem-Vindo ao meu App!</Text>
+            <Text>Bem-Vindo ao meu App! {nome}</Text>
             <Text>Contador: {count}</Text>
-            <Button title="Aumentar" onPress={()=> {setCount(count+1)}}/>
-            <Button title="Diminuir" onPress={()=> {setCount(count-1)}}/>
+            <Button title="Aumentar" onPress={()=> {setCount(count + 1)}}/>
+            <Button title="Diminuir" onPress={()=> {setCount(count - 1)}}/>
             <Button title="Resetar" onPress={()=> {setCount(STARTVALUE)}}/>
 
             <CustomButton title= "Name"/>
